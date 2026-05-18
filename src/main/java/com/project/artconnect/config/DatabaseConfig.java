63ca729ac9fc51eea2e7e56f3f0c2afb4f67345b
cleme <clementine.fleuret@efrei.net>
2026-05-18 22:12:30 +0200

    Mise en place de tous les deletes/add/updates + ajout d'informations dans les colonnes

--- src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -7,5 +7,5 @@ package com.project.artconnect.config;
 public class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/artconnect_db";
     public static final String USER = "root";
-    public static final String PASSWORD = "password"; // CHANGE ME
+    public static final String PASSWORD = "Clementine1Mysql!"; // CHANGE ME
 }
